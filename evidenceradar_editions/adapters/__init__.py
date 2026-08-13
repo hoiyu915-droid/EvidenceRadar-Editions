d278b4cd93cb447b32972febd41afadd98d09366
hoiyu915-droid <hoiyu915@gmail.com>
2026-08-14 01:35:05 +0800

    refactor: expose optional feed adapter

--- evidenceradar_editions/adapters/__init__.py
+++ evidenceradar_editions/adapters/__init__.py
@@ -1,6 +1,6 @@
 from .crossref import CrossrefAdapter
 from .europe_pmc import EuropePmcAdapter
 from .pubmed import PubMedAdapter
-from .radar_feed import RadarFeedAdapter
+from .radar_feed import RadarFeedAdapter as RadarRssAdapter
 
-__all__ = ["CrossrefAdapter", "EuropePmcAdapter", "PubMedAdapter", "RadarFeedAdapter"]
+__all__ = ["CrossrefAdapter", "EuropePmcAdapter", "PubMedAdapter", "RadarRssAdapter"]
