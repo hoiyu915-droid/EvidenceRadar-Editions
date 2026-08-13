5679f24dd38e5884f3c850ebb04ab266a8a3946c
hoiyu915-droid <hoiyu915@gmail.com>
2026-08-14 01:31:37 +0800

    feat: add source adapter package

---
+++ evidenceradar_editions/adapters/__init__.py
@@ -0,0 +1,6 @@
+from .crossref import CrossrefAdapter
+from .europe_pmc import EuropePmcAdapter
+from .pubmed import PubMedAdapter
+from .rss import RadarRssAdapter
+
+__all__ = ["CrossrefAdapter", "EuropePmcAdapter", "PubMedAdapter", "RadarRssAdapter"]
